0af94c0678d169d40682dcdf66ebcb637c94393a
BoyceLyu <lvzhendong.bl@gmail.com>
2016-04-27 09:15:57 +0800

    创建项目

---
+++ src/NursingHomes.Geteway/Geteway.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace NursingHomes.Geteway
+{
+    /// <summary>
+    /// FabricRuntime 为每个服务类型实例创建此类的一个实例。 
+    /// </summary>
+    internal sealed class Geteway : StatelessService
+    {
+        public Geteway(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// 可选择性地替代以创建此服务实例的侦听器(如 TCP、http)。
+        /// </summary>
+        /// <returns>侦听器集合。</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext => new OwinCommunicationListener(Startup.ConfigureApp, serviceContext, ServiceEventSource.Current, "ServiceEndpoint"))
+            };
+        }
+    }
+}
